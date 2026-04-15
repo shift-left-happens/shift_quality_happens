b458b47ef710bf73d7ab057a5651f717bef710c0
jarrald <novijgod@yahoo.dk>
2026-04-15 19:45:24 +0200

    cruds

--- src/main/java/dk/ek/shift_happens/shiftassignment/ShiftAssignmentController.java
+++ src/main/java/dk/ek/shift_happens/shiftassignment/ShiftAssignmentController.java
@@ -1,11 +1,10 @@
 package dk.ek.shift_happens.shiftassignment;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/shiftassignments")
@@ -17,5 +16,32 @@ public class ShiftAssignmentController {
     @GetMapping
     public List<ShiftAssignment> getShiftAssignments() {
         return this.shiftAssignmentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<ShiftAssignment> getShiftAssignmentById(@PathVariable Integer id) {
+        return this.shiftAssignmentRepository.findById(id);
+    }
+
+    @PostMapping
+    public ShiftAssignment createShiftAssignment(@RequestBody ShiftAssignment shiftAssignment) {
+        return this.shiftAssignmentRepository.save(shiftAssignment);
+    }
+
+    @PutMapping("/{id}")
+    public ShiftAssignment updateShiftAssignment(@PathVariable Integer id, @RequestBody ShiftAssignment shiftAssignmentDetails) {
+        ShiftAssignment shiftAssignment = this.shiftAssignmentRepository.findById(id).orElseThrow();
+        shiftAssignment.setShiftId(shiftAssignmentDetails.getShiftId());
+        shiftAssignment.setEmployeeId(shiftAssignmentDetails.getEmployeeId());
+        shiftAssignment.setAssignmentStatus(shiftAssignmentDetails.getAssignmentStatus());
+        shiftAssignment.setAssignedDatetime(shiftAssignmentDetails.getAssignedDatetime());
+        shiftAssignment.setCheckInDatetime(shiftAssignmentDetails.getCheckInDatetime());
+        shiftAssignment.setCheckOutDatetime(shiftAssignmentDetails.getCheckOutDatetime());
+        return this.shiftAssignmentRepository.save(shiftAssignment);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteShiftAssignment(@PathVariable Integer id) {
+        this.shiftAssignmentRepository.deleteById(id);
     }
 }
