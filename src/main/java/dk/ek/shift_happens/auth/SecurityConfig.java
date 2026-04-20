22a6a8b8dae823d026c8e8998dc520e5cd944cc6
jafar <jaal0002@stud.kea.dk>
2026-04-20 14:56:38 +0200

    .

--- src/main/java/dk/ek/shift_happens/auth/SecurityConfig.java
+++ src/main/java/dk/ek/shift_happens/auth/SecurityConfig.java
@@ -35,7 +35,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  */
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
