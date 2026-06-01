df6a28fdad502a3e5994c971c561824fe8634c13
jarrald <novijgod@yahoo.dk>
2026-06-01 14:46:01 +0200

    undo

--- src/main/java/dk/ek/shift_happens/jobrole/JobRoleController.java
+++ src/main/java/dk/ek/shift_happens/jobrole/JobRoleController.java
@@ -1,8 +1,6 @@
 package dk.ek.shift_happens.jobrole;
 
 import java.util.List;
-
-import dk.ek.shift_happens.department.DepartmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 public class JobRoleController {
 
     private final JobRoleService jobRoleService;
-    private final DepartmentService departmentService;
 
     @GetMapping
     @PreAuthorize("isAuthenticated()")
